372c7feb3d5faa3cc6bb6e302bbafbb1d77724a7
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-22 18:48:54 +0100

    Update settings.py

--- logistic/settings.py
+++ logistic/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'logistic/staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ORIGIN_WHITELIST = ['https//priestwelfareadmin.onrender.com',
+CORS_ORIGIN_WHITELIST = ['https//priestwelfare.netlify.app',
     #add other origins as needed
 ]
 
